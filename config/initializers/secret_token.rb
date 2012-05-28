f94b3bd78940a3d31fd03f65c76a382a7adf441b
Borys Rashba <rashba@gmail.com>
2012-05-28 10:48:24 -0700

    Fresh installation of refinerycms

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ign6::Application.config.secret_token = '785919290889115acc4333ecbdc427eacb4c32b9c23b0c86e6981c4c52c891bd01a530b521cfe45cda29ea23a62233dec73a4af37c9feef7b3069636a799fa90'

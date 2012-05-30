94e483e489ab05fbc442d4ff0b6a8b303758c80f
Borys Rashba <rashba@gmail.com>
2012-05-30 12:37:19 -0700

    Mass assign shop

--- vendor/extensions/shops/app/models/refinery/shops/shop.rb
+++ vendor/extensions/shops/app/models/refinery/shops/shop.rb
@@ -3,8 +3,8 @@ module Refinery
     class Shop < Refinery::Core::BaseModel
       self.table_name = 'refinery_shops'
 
-      attr_accessible :url, :position
-
+      attr_accessible :url, :position, :name, :short_description, :description, :working_time, :map, 
+                      :subcategory_id, :city, :phone, :e_mail, :logo_id, :vip_code
       acts_as_indexed :fields => [:url, :name, :short_description, :description, :city, :phone, :working_time]
       belongs_to :user
 #      validates :url, :presence => true, :uniqueness => true

24524dc7781d5f1b38586564c2bb68dc52842097
Borys Rashba <rashba@gmail.com>
2012-06-11 23:42:39 -0700

    S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Ign6::Application.configure do
 
   # Code is not reloaded between requests
   config.cache_classes = true
-
+  Refinery::Core.config.s3_backend = true
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true

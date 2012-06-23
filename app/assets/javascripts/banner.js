3f8c551772454c299197c01156e31101aa502de8
Borys Rashba <rashba@gmail.com>
2012-06-23 14:32:29 -0700

    Running Line, Banners, Show forms

--- app/assets/javascripts/banner.js
+++ app/assets/javascripts/banner.js
@@ -8,3 +8,23 @@ $(function(){
     hoverPause: true
   });
 });
+$(function(){
+	$('#slides2').slides({
+		preload: true,
+    pagination: true,
+    generatePagination: false,
+    play: 5000,
+    pause: 2500,
+    hoverPause: true
+  });
+});
+
+
+$(function(){
+  $('#running_line').crawlLine({
+    speed:2,
+    crawElement:'div', // для примера div.move
+    textElement:'p',
+    hoverClass:'viewText'
+  });
+});

046b6a18d748742afb858d49f21617f5022460cc
Borys Rashba <rashba@gmail.com>
2012-06-24 00:57:42 -0700

    Crawling line

--- app/assets/javascripts/banner.js
+++ app/assets/javascripts/banner.js
@@ -22,27 +22,39 @@ $(function(){
       
 $(document).ready(function() {
 
+if ($("#mvDiv").length > 0){
+
 var textStrLength = $('#mvDivP').width();
 var startPoint = $('#running_line').width();
 
 function crawlline () {
-  $('#mvDiv').css({left: startPoint + 1})
-	$('#mvDiv').animate({
-		left: 0 - textStrLength
-	},  (startPoint + 2 * textStrLength) * 10, 'linear', function () { 
-		crawlline();
-	}
-	)};
-	
-crawlline();
+  var pos = $('#mvDiv').position().left;
+  var duration = (pos + 2 * textStrLength) * 10;
+
+	$('#mvDiv').animate(
+    {left: 0 - textStrLength},
+    duration,
+    'linear', 
+    function () { 
+      $('#mvDiv').css({left: startPoint + 1})
+		  crawlline();
+	  }
+	)
+};
+  
+    crawlline();
 	
 	$('#mvDiv').hover(
 		function () {
-			$(this).stop();
+  		$(this).stop();
 		},
 		function () {
 			crawlline();
 		} 
 	)
+  
+}	
+
+
 	
 });

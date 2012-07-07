c53b2a7ad8700bf081b5f2fcadbd2d5d1f43dce5
Borys Rashba <rashba@gmail.com>
2012-07-07 13:51:13 -0700

    Minor changes Pravki2

--- app/controllers/refinery/sessions_controller.rb
+++ app/controllers/refinery/sessions_controller.rb
@@ -9,7 +9,8 @@ module Refinery
       resource = warden.authenticate!(auth_options)
       set_flash_message(:notice, :signed_in) if is_navigational_format?
       sign_in(resource_name, resource)
-      render :partial => "success"
+#      render :partial => "success"
+      redirect_back_or_default(refinery.root_path)
     rescue ::BCrypt::Errors::InvalidSalt, ::BCrypt::Errors::InvalidHash
       flash[:error] = t('password_encryption', :scope => 'refinery.users.forgot')
       render :partial => "new"

6e21dfb398dbc13a5a254a2d364097b76b658065
Borys Rashba <rashba@gmail.com>
2012-08-08 11:27:02 -0700

    Password editing

--- app/controllers/refinery/passwords_controller.rb
+++ app/controllers/refinery/passwords_controller.rb
@@ -47,5 +47,20 @@ module Refinery
         render :new
       end
     end
+    
+    
+    def update
+    self.resource = resource_class.reset_password_by_token(resource_params)
+
+    if resource.errors.empty?
+      flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
+      set_flash_message(:notice, flash_message) if is_navigational_format?
+      sign_in(resource_name, resource)
+      respond_with resource
+    else
+      respond_with resource
+    end
+  end
+    
   end
 end

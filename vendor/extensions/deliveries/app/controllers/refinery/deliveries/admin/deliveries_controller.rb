735f91cdd57411e435e0839a6e64dfe0d661c9c4
Borys Rashba <rashba@gmail.com>
2012-08-09 00:13:37 -0700

    Deliveries redone

--- vendor/extensions/deliveries/app/controllers/refinery/deliveries/admin/deliveries_controller.rb
+++ vendor/extensions/deliveries/app/controllers/refinery/deliveries/admin/deliveries_controller.rb
@@ -10,13 +10,61 @@ module Refinery
         def create 
           @delivery = Delivery.new(params[:delivery])
           if @delivery.save
-            @delivery.send_a_post
+            send_a_post(@delivery)
             redirect_to refinery.deliveries_admin_deliveries_url
           else
             render action: "new"
           end
         end                
-                
+           
+        
+        
+        
+        def send_a_post(delivery)
+        if delivery.entity == "users" then
+          @entities = Refinery::User.where('city LIKE ?', "%#{delivery.city}%")
+          
+          @entities.each do |en|        
+            DeliveryMailer.package_to_send_user(delivery, en)
+          end
+          
+          
+        elsif delivery.entity == "companies" then
+          if !delivery.service_id.nil? then 
+            @entities = Refinery::Services::Service.find(delivery.service_id).companies.where('city LIKE ?', "%#{delivery.city}%")
+          elsif !delivery.category_id.nil? then
+            @entities = Refinery::Companies::Company.where('city LIKE ?', "%#{delivery.city}%")
+          end
+          
+          @entities.each do |en|        
+            DeliveryMailer.package_to_send(delivery, en)
+          end
+          
+          
+        elsif delivery.entity == "shops" then
+          if !delivery.subcategory_id.nil? then 
+            @entities = Refinery::Subcategories::Subcategory.find(delivery.subcategory_id).shops.where('city LIKE ?', "%#{delivery.city}%")
+          elsif !self.category_id.nil? then
+            @entities = Refinery::Categories::Category.find(delivery.category_id).shops.where('city LIKE ?', "%#{delivery.city}%")
+          else
+            @entities = Refinery::Shops.where('city LIKE ?', "%#{delivery.city}%")
+          end
+          
+          
+          @entities.each do |en|        
+            DeliveryMailer.package_to_send(delivery, en)
+          end
+          
+          
+        end
+        
+        delivery.status = 'done'
+        delivery.save!
+        
+      end
+        
+        
+        
       end
     end
   end
--- vendor/extensions/deliveries/app/models/refinery/deliveries/delivery.rb
+++ vendor/extensions/deliveries/app/models/refinery/deliveries/delivery.rb
@@ -10,46 +10,6 @@ module Refinery
       validates :entity, :presence => true
       validates_inclusion_of :entity, :in => Delivery::ENTITIES
       
-      def send_a_post
-        self.status = 'done'
-        self.save!
-        if self.entity == "users" then
-          @entities = Refinery::User.where('city LIKE ?', "%#{self.city}%")
-          
-          @entities.each do |en|        
-            DeliveryMailer.package_to_send_user(self, en)
-          end
-          
-          
-        elsif self.entity == "companies" then
-          if !self.service_id.nil? then 
-            @entities = Refinery::Services::Service.find(self.service_id).companies.where('city LIKE ?', "%#{self.city}%")
-          elsif !self.category_id.nil? then
-            @entities = Refinery::Companies::Company.where('city LIKE ?', "%#{self.city}%")
-          end
-          
-          @entities.each do |en|        
-            DeliveryMailer.package_to_send(self, en)
-          end
-          
-          
-        elsif self.entity == "shops" then
-          if !self.subcategory_id.nil? then 
-            @entities = Refinery::Subcategories::Subcategory.find(self.subcategory_id).shops.where('city LIKE ?', "%#{self.city}%")
-          elsif !self.category_id.nil? then
-            @entities = Refinery::Categories::Category.find(self.category_id).shops.where('city LIKE ?', "%#{self.city}%")
-          else
-            @entities = Refinery::Shops.where('city LIKE ?', "%#{self.city}%")
-          end
-          
-          
-          @entities.each do |en|        
-            DeliveryMailer.package_to_send(self, en)
-          end
-          
-          
-        end
-      end
     end
   end
 end

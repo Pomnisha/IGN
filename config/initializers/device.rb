43bad40637f52ead1dbbf2e772f9a541eb83cf5f
Borys Rashba <rashba@gmail.com>
2012-08-22 17:40:53 -0700

    mailer

--- config/initializers/device.rb
+++ config/initializers/device.rb
@@ -2,6 +2,6 @@ Devise.setup do |config|
   config.http_authenticatable_on_xhr = false
   config.navigational_formats = [:html, :json]
 #  config.mailer = "Ign6::Application::Mailer"
-#  config.mailer_sender = "admin@ign.su"
+  config.mailer_sender = "admin@ign.su"
 
 end#
